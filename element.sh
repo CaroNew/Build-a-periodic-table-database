83c4e36ba347503e203216a658cb947d3df46ee8
Carolina Lucero <mateconpeperina23@gmail.com>
2024-08-08 15:36:25 +0000

    feat: quering tables

--- element.sh
+++ element.sh
@@ -6,7 +6,17 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --no-align --tuple
 
 if [[ $1 ]] 
 then 
-  SELECT_RESULT=$($PSQL "SELECT * FROM properties INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")
+  #if is a number
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    # searching atomic number
+    SELECT_RESULT=$($PSQL "SELECT * FROM properties INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")
+  else
+    # searching element symbol/name
+    SELECT_RESULT=$($PSQL "SELECT * FROM properties INNER JOIN elements USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$1' or name='$1'")
+  fi
+
+
   echo "$SELECT_RESULT" | while IFS="|" read TYPE_ID ATOMIC_NUMBER ATOMIC_MASS MELTING_POINT BOILING_POINT SYMBOL ELEMENT_NAME TYPE_NAME
   do
     echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($SYMBOL). It's a $TYPE_NAME, with a mass of $ATOMIC_MASS amu. Hydrogen has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius.\n"
