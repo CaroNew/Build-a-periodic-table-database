692cfb38b5f75081a6170bf12784e1ab7c08da74
Carolina Lucero <mateconpeperina23@gmail.com>
2024-08-08 15:23:12 +0000

    feat: set main message

--- element.sh
+++ element.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 ]] 
 then 
-    echo -e "\nhello\n"
+    echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($SYMBOL). It's a $TYPE_NAME, with a mass of $ATOMIC_MASS amu. Hydrogen has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius.\n"
 else
     echo "Please provide an element as an argument."
 fi
